10f998646fc958c843ddb82d7b11a5d96cc89a7b
Deep PS <33302767+DonnyPS@users.noreply.github.com>
2024-01-03 05:36:56 +0700

    Add files via upload

---
+++ cypress/pages/LoginPage.js
@@ -0,0 +1,20 @@
+class LoginPage {
+    visit() {
+      cy.visit('/customer/account/login/')
+    }
+  
+    fillEmail(email) {
+      cy.get('#email').type(email)
+    }
+  
+    fillPassword(password) {
+      cy.get('#pass').type(password)
+    }
+  
+    submit() {
+      cy.get('#send2').click()
+    }
+  }
+  
+  export default LoginPage
+  
