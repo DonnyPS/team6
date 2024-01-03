f3d2327c7bba06e21694c89de613510b4df77607
inayatimachsus <inayati.mia@gmail.com>
2024-01-03 22:06:23 +0700

    Edit email and password

--- cypress/e2e/edit_akun.cy.js
+++ cypress/e2e/edit_akun.cy.js
@@ -8,4 +8,31 @@ describe('Edit Account Information Test', () => {
       // Assertion
       cy.get('.message-success').should('contain', 'You saved the account information.')
     })
+})
+
+describe('Edit Email', () => {
+  it('Successfully edits email information', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
+    cy.get('checkbox[title="change-email"]').click()
+    cy.get('#email').clear().type('newemail@example.com')
+    cy.get('#current-password').clear().type('Password123')
+    cy.get('button[title="Save"]').click()
+
+    // Assertion
+    cy.get('.message-success').should('contain', 'You saved the account information.')
   })
+})
+
+describe('Edit Password', () => {
+  it('Successfully edits email information', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
+    cy.get('checkbox[title="change-password"]').click()
+    cy.get('#current-password').clear().type('Password123')
+    cy.get('#password').clear().type('123Password')
+    cy.get('#password-confirmation').clear().type('123Password')
+    cy.get('button[title="Save"]').click()
+
+    // Assertion
+    cy.get('.message-success').should('contain', 'You saved the account information.')
+  })
+})
