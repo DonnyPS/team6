3aa090894dd823ecde65ecb423781fd7e28638aa
inayatimachsus <inayati.mia@gmail.com>
2024-01-04 13:27:59 +0700

    melengkapi edit address

--- cypress/e2e/edit_address.cy.js
+++ cypress/e2e/edit_address.cy.js
@@ -4,6 +4,9 @@ describe('Edit Address Test', () => {
   
       cy.get('#street_1').clear().type('123 New Street')
       cy.get('#city').clear().type('New City')
+      cy.get('select#region_id option:selected').should('have.value', '62')
+      cy.get('#zip').clear().type('98831')
+      cy.get('select#country option:selected').should('have.value', 'US')
   
       cy.get('button[title="Save Address"]').click()
   
--- cypress/e2e/edit_akun.cy.js
+++ cypress/e2e/edit_akun.cy.js
@@ -1,6 +1,9 @@
 describe('Edit Account Information Test', () => {
-    it('Successfully edits account information', () => {
+    beforeEach(() => {
       cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
+    })
+
+    it('Successfully edits account information', () => {
       cy.get('#firstname').clear().type('NewFirstName')
       cy.get('#lastname').clear().type('NewLastName')
       cy.get('button[title="Save"]').click()
@@ -8,31 +11,25 @@ describe('Edit Account Information Test', () => {
       // Assertion
       cy.get('.message-success').should('contain', 'You saved the account information.')
     })
-})
 
-describe('Edit Email', () => {
-  it('Successfully edits email information', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
-    cy.get('checkbox[title="change-email"]').click()
-    cy.get('#email').clear().type('newemail@example.com')
-    cy.get('#current-password').clear().type('Password123')
-    cy.get('button[title="Save"]').click()
+    it('Successfully edits email information', () => {
+      cy.get('checkbox[title="Change Email"]').click()
+      cy.get('#email').clear().type('newemail@example.com')
+      cy.get('#current-password').clear().type('Password123')
+      cy.get('button[title="Save"]').click()
+  
+      // Assertion
+      cy.get('.message-success').should('contain', 'You saved the account information.')
+    })
 
-    // Assertion
-    cy.get('.message-success').should('contain', 'You saved the account information.')
-  })
-})
-
-describe('Edit Password', () => {
-  it('Successfully edits email information', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
-    cy.get('checkbox[title="change-password"]').click()
-    cy.get('#current-password').clear().type('Password123')
-    cy.get('#password').clear().type('123Password')
-    cy.get('#password-confirmation').clear().type('123Password')
-    cy.get('button[title="Save"]').click()
-
-    // Assertion
-    cy.get('.message-success').should('contain', 'You saved the account information.')
-  })
+    it('Successfully edits email information', () => {
+      cy.get('checkbox[title="Change Password"]').click()
+      cy.get('#current-password').clear().type('Password123')
+      cy.get('#password').clear().type('123Password')
+      cy.get('#password-confirmation').clear().type('123Password')
+      cy.get('button[title="Save"]').click()
+  
+      // Assertion
+      cy.get('.message-success').should('contain', 'You saved the account information.')
+    })
 })
